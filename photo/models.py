e9dace017329b5b90dc28750d2806c41d8f477e2
layersony <sammaingi5@gmail.com>
2021-07-01 13:06:07 +0300

    model image add

--- photo/models.py
+++ photo/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='photos')
+  image_name =  models.CharField(max_length=200)
+  image_desc = models.TextField()
+  location_id = models.ForeignKey
+  date_upload = models.DateTimeField(auto_now_add=True)
+
 
