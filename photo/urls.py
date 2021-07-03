b5b54e96434902ecea5fd21ea77f11748f8db311
layersony <sammaingi5@gmail.com>
2021-07-03 15:36:27 +0300

    add 2 urls seatch location

--- photo/urls.py
+++ photo/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='home')
+    url(r'^$', views.index, name='home'), 
+    url(r'^search/', views.search_photo_category, name='search_results'), 
+    url(r'^location/', views.location, name='locate')
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
