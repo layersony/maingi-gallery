213ebd6df11222227700e5e272fc9984981830d5
layersony <sammaingi5@gmail.com>
2021-07-03 19:04:09 +0300

    delete update method for location pass

--- photo/tests.py
+++ photo/tests.py
@@ -64,11 +64,23 @@ class TestLocation(TestCase):
   def test_instance(self):
     self.assertTrue(isinstance(self.location, Location))
 
-
   def test_saveLocation(self):
     self.location.saveLocation()
     location = Location.objects.all()
     self.assertTrue(len(location)>0)
+
+  def test_deleteLocation(self):
+    self.location.saveLocation()
+    self.location2 = Location.objects.create(location='USA')
+    Location.deleteLocation(self.location2.id)
+    self.assertTrue(len(Location.objects.all())==1)
+
+  def test_updateLocation(self):
+    update_term = 'USA'
+    self.location.saveLocation()
+    Location.updateLocation(self.location.id, update_term)  
+    updated_one = Location.objects.get(id=self.location.id)
+    self.assertEqual(updated_one.location, 'USA')
 
 class TestCategory(TestCase):
   def setUp(self):
