5c1a28cecec34871dc2a0d014a729052b58217e0
layersony <sammaingi5@gmail.com>
2021-07-03 19:01:51 +0300

    update and delete methods add

--- photo/models.py
+++ photo/models.py
@@ -6,6 +6,13 @@ class Location(models.Model):
   def saveLocation(self):
     self.save()
 
+  @classmethod
+  def deleteLocation(cls, id):
+    cls.objects.filter(id=id).delete()
+
+  @classmethod
+  def updateLocation(cls, id, locaUpdate):
+    cls.objects.filter(id=id).update(location=locaUpdate)
   def __str__(self):
     return self.location
 
